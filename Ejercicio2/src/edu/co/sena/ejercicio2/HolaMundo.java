8f7d3b9f525e8f5a02aa14f92f20ec1c8673155d
Nicolas <ndhernandez2@misena.edu.co>
2016-02-18 21:28:26 -0500

    commir hola mundo

---
+++ Ejercicio2/src/edu/co/sena/ejercicio2/HolaMundo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.co.sena.ejercicio2;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author NICO-DAVID
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Hola mundo");
+    }
+    
+}
